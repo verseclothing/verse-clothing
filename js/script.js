84bc2e56cc9abd6939e9220878206fb510e6708f
verseclothing <veeryadavmba@gmail.com>
2026-06-20 01:09:08 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -26,3 +26,32 @@ count.innerText = cart.length;
 }
 
 }
+function loadCart(){
+
+let cart = JSON.parse(localStorage.getItem("cart")) || [];
+
+let cartItems = document.getElementById("cart-items");
+let total = document.getElementById("cart-total");
+
+if(!cartItems) return;
+
+cartItems.innerHTML = "";
+
+let grandTotal = 0;
+
+cart.forEach((item,index)=>{
+
+grandTotal += item.price;
+
+cartItems.innerHTML += `
+<div class="card" style="margin-bottom:20px;">
+    <h3>${item.name}</h3>
+    <p>₹${item.price}</p>
+</div>
+`;
+
+});
+
+total.innerText = "Total: ₹" + grandTotal;
+
+}
