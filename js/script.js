c59e4ed011d8d0c12a07d4b40b2a0bc35ad0466d
verseclothing <veeryadavmba@gmail.com>
2026-06-20 00:59:31 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,28 @@
-console.log("Verse Clothing Co. Loaded");
+function addToCart(name, price){
+
+let cart = JSON.parse(localStorage.getItem("cart")) || [];
+
+cart.push({
+name: name,
+price: price
+});
+
+localStorage.setItem("cart", JSON.stringify(cart));
+
+document.getElementById("cart-count").innerText = cart.length;
+
+alert("✅ Product Added to Cart");
+
+}
+
+window.onload = function(){
+
+let cart = JSON.parse(localStorage.getItem("cart")) || [];
+
+let count = document.getElementById("cart-count");
+
+if(count){
+count.innerText = cart.length;
+}
+
+}
